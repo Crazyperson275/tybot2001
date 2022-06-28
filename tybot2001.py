f9a4a1a1d333e9c8fca5b3f52340bb7f72795627
Tyler <tyler.edwin.johnson@gmail.com>
2022-06-28 07:56:01 -0600

    intial code

---
+++ tybot2001.py
@@ -0,0 +1,139 @@
+from tools import *
+from objects import *
+from routines import *
+from tmcp import TMCPHandler, TMCPMessage, ActionType
+
+
+#This file is for strategy
+class ExampleBot(GoslingAgent):
+    def __init__(self, name, team, index):
+        super().__init__(name, team, index)
+        self.step = None
+
+    def get_boost(agent):
+
+        large_boosts = [boost for boost in agent.boosts if boost.large and boost.active]
+        closest = large_boosts[0]
+        closest_distance = (large_boosts[0].location - agent.me.location).magnitude()
+        for item in large_boosts:
+
+            item_distance = (item.location - agent.me.location).magnitude()
+            if item_distance < closest_distance:
+                closest = item
+
+                closest_distance = item_distance
+        agent.push(goto_boost(closest))
+
+    def goal_boost(agent):
+        boosts = [boost for boost in agent.boosts if boost.large and boost.active and abs(
+            agent.friend_goal.location.y - boost.location.y) - 200 < abs(agent.friend_goal.location).magnitude()]
+
+        if len(boosts) > 0:
+            closest = boosts[0]
+
+            for boost in boosts:
+
+                if (boost.location - agent.me.location).magnitude() < (closest.location - agent.me.location).magnitude():
+                    closest = boost
+            agent.push(goto_boost(closest, agent.friend_goal.location))
+
+    def shots(agent, ball_right, ball_left, left_side, right_side):
+
+        left_back_post = False
+        right_back_post = False
+
+        targets = {"goal": (left_side, right_side)}
+        shots = find_hits(agent, targets)
+
+        if len(shots["goal"]) > 0:
+            agent.push(shots["goal"][0])
+
+        else:
+            if ball_right:
+                left_back_post = True
+            elif ball_left:
+                right_back_post = True
+        return left_back_post, right_back_post
+
+    def run(agent):
+
+        foe_close_to_net = (agent.foes[0].location - agent.friend_goal.location).magnitude() < 1500
+
+        left_post = agent.friend_goal.left_post + Vector3(0,500,0) * -side(agent.team)
+        right_post = agent.friend_goal.right_post + Vector3(0,500,0) * -side(agent.team)
+        ball_right = agent.ball.location.x >= 0
+        ball_left = agent.ball.location.x < 0
+
+        foe_right_side = agent.foes[0].location.x >= 0
+        foe_left_side = agent.foes[0].location.x < 0
+
+        my_goal_to_ball, my_ball_distance = (agent.ball.location - agent.friend_goal.location).normalize(True)
+        goal_to_me = agent.me.location - agent.friend_goal.location
+        my_distance = my_goal_to_ball.dot(goal_to_me)
+
+        foe_goal_to_ball, foe_ball_distance = (agent.ball.location - agent.foe_goal.location).normalize(True)
+        foe_goal_to_foe = agent.foes[0].location - agent.foe_goal.location
+        foe_distance = foe_goal_to_ball.dot(foe_goal_to_foe)
+
+        me_onside = my_distance - 200 < my_ball_distance
+        foe_onside = foe_distance - 200 < foe_ball_distance
+
+        super_close = (agent.me.location - agent.ball.location).magnitude() < 750
+        close = (agent.me.location - agent.ball.location).magnitude() < 3000
+        foe_super_close = (agent.foes[0].location - agent.ball.location).magnitude() < 750
+        foe_close = (agent.foes[0].location - agent.ball.location).magnitude() < 3000
+        have_boost = agent.me.boost > 10
+
+        left_back_post = False
+        right_back_post = False
+        return_to_goal = False
+
+        if agent.team == 0:
+            agent.debug_stack()
+            agent.line(agent.friend_goal.location, agent.ball.location, [255, 255, 255])
+            my_point = agent.friend_goal.location + (my_goal_to_ball * my_distance)
+            agent.line(my_point - Vector3(0, 0, 100), my_point + Vector3(0, 0, 100), [0, 255, 0])
+
+        if len(agent.stack) < 1:
+            if agent.kickoff_flag:
+                agent.push(kickoff(agent))
+
+            elif foe_onside and not me_onside and foe_close:
+                if ball_right:
+                    left_back_post = True
+                elif ball_left:
+                    right_back_post = True
+
+            elif not foe_onside and close and me_onside and not foe_close:
+                left_back_post, right_back_post = agent.shots(ball_right, ball_left, agent.foe_goal.left_post, agent.foe_goal.right_post)
+
+            elif me_onside and not foe_close and foe_right_side and foe_onside:
+                left_back_post, right_back_post = agent.shots(ball_right, ball_left, agent.foe_goal.left_post, agent.foe_goal.location)
+
+            elif me_onside and not foe_close and foe_left_side and foe_onside:
+                left_back_post, right_back_post = agent.shots(ball_right, ball_left, agent.foe_goal.location, agent.foe_goal.right_post)
+
+            elif not me_onside and not have_boost and not foe_close:
+                agent.goal_boost()
+
+            elif not have_boost and not foe_close:
+                agent.get_boost()
+
+            else:
+                left_back_post, right_back_post = agent.shots(ball_right, ball_left, agent.foe_goal.left_post, agent.foe_goal.right_post)
+
+        if return_to_goal:
+            distance_to_goal = abs(agent.friend_goal.location.y) - abs(agent.me.location.y)
+            relative_target = agent.friend_goal.location - agent.me.location
+            angles = defaultPD(agent, agent.me.local(relative_target))
+            defaultThrottle(agent, 2300)
+            agent.controller.boost = False if abs(angles[1]) > .5 or agent.me.airborne else agent.controller.boost
+            agent.controller.handbrake = True if abs(angles[1]) > 2.8 else False
+
+        if left_back_post:
+            print("left back post")
+            agent.push(goto(left_post, agent.ball.location))
+
+        if right_back_post:
+            print("right back post")
+            agent.push(goto(right_post, agent.ball.location))
